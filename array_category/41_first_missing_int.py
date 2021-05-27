8f57526749ca10650a377324862e27cfb73fa4e3
lifangfu88 <lifangfu88@gmail.com>
2021-05-26 23:42:08 -0700

    Add more comments.

--- array_category/41_first_missing_int.py
+++ array_category/41_first_missing_int.py
@@ -10,15 +10,30 @@ def first_missing(nums: List[int]):
     3. to make O(1) space, need to utilize the list index as hash key,
     put the element in the correct position
     utilize the index
+
+    val:   3, 4, -1, 2, 3
+    index: 0, 1,  2, 3, 4
+
+    #1:   3, 4,  3, 2, 3
+    #2:   -1, 2,  3, 4, 3
+    #3:   -1, 2,  3, 4, 3
+    #4:   -1, 2,  3, 4, 3
+    #5:   -1, 2,  3, 4, 3
+
     """
     n = len(nums)
     for i in range(n):
-        while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
-            nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
+        val = nums[i]
+        # only keep val that is in [1, n]
+        while 1 <= val <= n and nums[val - 1] != val:
+            # assign val to correct index
+            nums[val - 1], val = val, nums[val - 1]
     for i in range(n):
         if nums[i] != i + 1:
             return i + 1
     return n + 1
 
+if __name__ == '__main__':
+    print(first_missing([3, 4, 1, 8, 2, 2, 3]))
 
 
