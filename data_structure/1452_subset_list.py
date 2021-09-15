e880cee838061cb2b7afec10c65556b23e67f0e4
lifangfu88 <lifangfu88@gmail.com>
2021-09-14 21:36:10 -0700

    1452, 283

---
+++ data_structure/1452_subset_list.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def peopleIndexes(self, favoriteCompanies: List[List[str]]) -> List[int]:
+        i_to_f = {}
+        for i in range(len(favoriteCompanies)):
+            i_to_f[i] = set(favoriteCompanies[i])
+        sub = set()
+        for i in i_to_f.keys():
+            for j in i_to_f.keys():
+                if i != j and i_to_f[i].issubset(i_to_f[j]):
+                    sub.add(i)
+                    break
+        res = []
+        for i in range(len(favoriteCompanies)):
+            if i not in sub:
+                res.append(i)
+
+        return res
